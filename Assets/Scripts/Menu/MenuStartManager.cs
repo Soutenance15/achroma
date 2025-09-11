50e7e9dcf44bd5c12a559f1968e5da27ca95829c
Soutenance15 <soutenance15@proton.me>
2025-09-11 08:47:49 +0400

    Reset to clean Project

--- Assets/Scripts/Menu/MenuStartManager.cs
+++ Assets/Scripts/Menu/MenuStartManager.cs
@@ -8,9 +8,14 @@ public class MenuStartManager : MonoBehaviour
     Button showDialogQuitButton;
     Button cancelQuitButton;
     Button confirmQuitButton;
+    Button menuLevelButton;
+
+    private GameObject menuLevelInstance = null;
 
     void Awake()
     {
+        menuLevelButton = GameObject.Find("MenuLevel").GetComponent<Button>();
+
         // Trouver les objets dans la hiérarchie par nom
         quitDialog = GameObject.Find("QuitDialog");
         showDialogQuitButton = GameObject.Find("ShowDialogQuitButton").GetComponent<Button>();
@@ -21,9 +26,32 @@ public class MenuStartManager : MonoBehaviour
         quitDialog.SetActive(false);
 
         // Ajouter les listeners par code
+
+        menuLevelButton.onClick.AddListener(ShowMenuLevel);
+
         showDialogQuitButton.onClick.AddListener(ShowQuitDialog);
         confirmQuitButton.onClick.AddListener(QuitApp);
         cancelQuitButton.onClick.AddListener(HideQuitDialog);
+    }
+
+    void ShowMenuLevel()
+    {
+        if (menuLevelInstance == null)
+        {
+            // Charge et instancie le prefab la première fois
+            GameObject prefab = Resources.Load<GameObject>("Prefabs/MenuLevel");
+            if (prefab != null)
+            {
+                menuLevelInstance = Instantiate(prefab);
+            }
+            else
+                Debug.LogError("menuLevel prefab non trouvé dans Resources/Prefabs !");
+        }
+        else
+        {
+            // Si déjà instancié (cache/réaffiche selon besoin, par ex avec SetActive)
+            menuLevelInstance.SetActive(true);
+        }
     }
 
     void ShowQuitDialog()
